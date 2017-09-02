df4a14ecac266a9edc8af88c0eb196c412a500bd
Joe Crescenzi <CouponPages@gmail.com>
2017-09-02 08:59:00 -0400

    New ViewController for the Add Page.

---
+++ ToDo List Lesson/AddPageViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AddPageViewController.swift
+//  ToDo List Lesson
+//
+//  Created by Joe Crescenzi on 9/2/17.
+//  Copyright © 2017 Joe Crescenzi. All rights reserved.
+//
+
+import UIKit
+
+class AddPageViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var TitleTextBox: UITextField!
+    
+    @IBOutlet weak var IsImportantToggle: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func AddButtonWasTapped(_ sender: Any) {
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+     
+     
+     override func didReceiveMemoryWarning() {
+     super.didReceiveMemoryWarning()
+     // Dispose of any resources that can be recreated.
+     }
+    */
+
+}
