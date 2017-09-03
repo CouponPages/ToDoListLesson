aac6479ba1f14667a33648341b90f3b6c12d45e4
Joe Crescenzi <CouponPages@gmail.com>
2017-09-02 21:51:10 -0400

    Removed some unused code... finished.

--- ToDo List Lesson/AddPageViewController.swift
+++ ToDo List Lesson/AddPageViewController.swift
@@ -40,37 +40,6 @@ class AddPageViewController: UIViewController {
             }
         }
         
-        
-        /*
-        let MyTodo = ToDoClass()
-        MyTodo.IsImportant = IsImportantToggle.isOn
-        if let NewTitle = TitleTextBox.text {
-            if NewTitle != "" {
-                MyTodo.Name = NewTitle
-                myPreviousVC.toDoList.append(MyTodo)
-                myPreviousVC.tableView.reloadData()
-                navigationController?.popViewController(animated: true)
-
-            }
-        }
-        */
-        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-     
-     
-     override func didReceiveMemoryWarning() {
-     super.didReceiveMemoryWarning()
-     // Dispose of any resources that can be recreated.
-     }
-    */
 
 }
--- ToDo List Lesson/DetailViewController.swift
+++ ToDo List Lesson/DetailViewController.swift
@@ -28,7 +28,7 @@ class DetailViewController: UIViewController {
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
             if let theSelectedToDo = selectedToDo {
                 context.delete(theSelectedToDo)
-                try? context.save() 
+                try? context.save()
                 navigationController?.popViewController(animated: true)
             }
         
@@ -40,17 +40,5 @@ class DetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
